777563a3cd26279465b09d64f4d9b5f198a31e02
Darioush Mansourzadeh <darioush2011@gmail.com>
2019-09-25 22:36:42 -0700

    updated portfolio

---
+++ assets/index.js
@@ -0,0 +1,24 @@
+$('#portfolio-photos1').on('click', function() {
+    
+    window.open('https://github.com/Darioush1/ballProject2')
+});
+
+$('#portfolio-photos2').on('click', function() {
+    
+    window.open('https://github.com/Darioush1/Trivia-Game')
+});
+
+$('#portfolio-photos3').on('click', function() {
+    
+    window.open('https://github.com/Darioush1/RPS-Miltiplayer')
+});
+
+$('#portfolio-photos4').on('click', function() {
+    
+    window.open('https://github.com/Darioush1/unit-4-game')
+});
+
+$('#portfolio-photos3').on('click', function() {
+    
+    window.open('https://github.com/Darioush1/Pyschic-Game')
+});
